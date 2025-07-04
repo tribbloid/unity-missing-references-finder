1d3e9edda3e6339bc933021a56fe62d237b8e78e
Peng Cheng <peng.cheng@acyclic.ai>
2025-07-04 18:35:41 -0400

    reformat

--- Editor/MissingPrefabsFinder.cs
+++ Editor/MissingPrefabsFinder.cs
@@ -2,9 +2,10 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class MissingPrefabsFinder { // Based on this post: https://forum.unity.com/threads/detecting-missing-nested-prefab.697562/
+public class MissingPrefabsFinder {
+	// Based on this post: https://forum.unity.com/threads/detecting-missing-nested-prefab.697562/
 	[MenuItem("Tools/Find Missing Prefabs", false, 50)]
-	static void Init() {
+	private static void Init() {
 		var allPrefabs = GetAllPrefabs();
 
 		var count = 0;
@@ -25,22 +26,18 @@ public class MissingPrefabsFinder { // Based on this post: https://forum.unity.c
 				FindMissingPrefabInGO(go, prefab, true);
 
 				GameObject.DestroyImmediate(go);
-
 			} catch {
 				Debug.Log("For some reason, prefab " + prefab + " won't cast to GameObject");
-
 			}
 		}
 
 		EditorUtility.ClearProgressBar();
 	}
 
-
-	static void FindMissingPrefabInGO(GameObject g, string prefabName, bool isRoot) {
+	private static void FindMissingPrefabInGO(GameObject g, string prefabName, bool isRoot) {
 		if (g.name.Contains("Missing Prefab")) {
 			Debug.LogError($"{prefabName} has missing prefab {g.name}");
 			return;
-
 		}
 
 		if (PrefabUtility.IsPrefabAssetMissing(g)) {
@@ -48,39 +45,38 @@ public class MissingPrefabsFinder { // Based on this post: https://forum.unity.c
 			return;
 		}
 
-		#if !UNITY_2021_3_OR_NEWER
+#if !UNITY_2021_3_OR_NEWER
 		if (PrefabUtility.IsDisconnectedFromPrefabAsset(g)) {
 			Debug.LogError($"{prefabName} has missing prefab {g.name}");
 			return;
 		}
-		#endif
+#endif
 
 		if (!isRoot) {
-			if (PrefabUtility.IsAnyPrefabInstanceRoot(g)) {
+			if (PrefabUtility.IsAnyPrefabInstanceRoot(g))
 				return;
-			}
 
-			GameObject root = PrefabUtility.GetNearestPrefabInstanceRoot(g);
-			if (root == g) {
+			var root = PrefabUtility.GetNearestPrefabInstanceRoot(g);
+			if (root == g)
 				return;
-			}
 		}
-
 
 		// Now recurse through each child GO (if there are any):
-		foreach (Transform childT in g.transform) {
+		foreach (Transform childT in g.transform)
+
 			//Debug.Log("Searching " + childT.name  + " " );
 			FindMissingPrefabInGO(childT.gameObject, prefabName, false);
-		}
 	}
 
 	public static string[] GetAllPrefabs() {
-		string[] temp = AssetDatabase.GetAllAssetPaths();
-		List<string> result = new List<string>();
-		foreach (string s in temp) {
-			if (s.Contains(".prefab")) result.Add(s);
+		var temp = AssetDatabase.GetAllAssetPaths();
+		var result = new List<string>();
+		foreach (var s in temp)
+			if (s.Contains(".prefab"))
+				result.Add(s);
 		}
 
 		return result.ToArray();
 	}
+
 }
